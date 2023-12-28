fdf25e1ce85b7792b346452cad96ed79764b643c
Aashish Gulshan <aashish.gulshan@zehntech.com>
2023-12-28 13:04:40 +0530

    User Registration controller created, but cover image not upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,7 @@ import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // ---Get user Details from frontend---
-  const { userName, email, fullName, password } = req.body;
+  const { username, email, fullName, password } = req.body;
 
   // ------------------------------ Method 1 to validate the fiels --------------
   //   if (fullName === "") {
@@ -38,34 +38,44 @@ const registerUser = asyncHandler(async (req, res) => {
   // ------------------------------ Method 2 to validate the fiels --------------
 
   if (
-    [fullName, email, userName, password].some((field) => field?.trim() === "")
+    [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All feilds Required !!");
   }
 
-  const existedUser = User.findOne({
-    $or: [{ userName }, { email }],
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
   });
 
   if (existedUser) {
     throw new ApiError(409, "User with email or username already register");
   }
+  console.log(req.files);
 
-  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const avtarLocalPath = req.files?.avtar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  if (!avatarLocalPath) {
+  // let coverImageLocalPath;
+  // if (
+  //   req.files &&
+  //   Array.isArray(req.files.coverImage) &&
+  //   req.files.coverImage.length > 0
+  // ) {
+  //   coverImageLocalPath = req.files.coverImage[0].path;
+  // }
+
+  if (!avtarLocalPath) {
     throw new ApiError(400, " Avtar file is required ");
   }
   // if cover image is required then
   // if (!coverImageLocalPath) {
   //   throw new ApiError(400, " Cover file is required ");
   // }
-  const avtar = await uploadOnCloudinary(avatarLocalPath);
+  const avtar = await uploadOnCloudinary(avtarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avtar) {
-    throw new ApiError(400, "Avtar file is required");
+    throw new ApiError(400, "avtar file is required");
   }
 
   const user = await User.create({
@@ -74,23 +84,28 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage?.url || "",
     email,
     password,
-    userName: userName.toLowerCase,
+    username: username.toLowerCase(),
   });
+
+  console.log(user);
 
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
-  )
+  );
 
-  if(!createdUser) {
-    throw new ApiError (500, " Something went wrong while registration of the user")
+  if (!createdUser) {
+    throw new ApiError(
+      500,
+      " Something went wrong while registration of the user"
+    );
   }
 
-  return res.status(201).json(
-    new ApiResponse(200, createdUser, " user registered succesfully")
-  )
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, " user registered succesfully"));
 
   //   console.log("Full Name:- ", fullName);
-  //   console.log("User Name:- ", userName);
+  //   console.log("User Name:- ", username);
   //   console.log("Email:- ", email);
 
   res.status(200).json({
@@ -100,3 +115,34 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 export { registerUser };
+
+
+
+/*
+ISSUE:=>
+        [1]. When i try to upload avtar image then it works fine but whenever i try to uplopad avtar with cover image then it showing:-
+        -------------------------------------------------
+        [nodemon] starting `node -r dotenv/config --experimental-json-modules src/index.js`
+
+ MongoDB Connected Sucessfully !! DB HOST: ac-k4fitwn-shard-00-01.cumxf6g.mongodb.net
+ Server is running on port: 8000
+[Object: null prototype] {
+  avtar: [
+    {
+      fieldname: 'avtar',
+      originalname: 'hsf.jpg',
+      encoding: '7bit',
+      mimetype: 'image/jpeg',
+      destination: './public/temp',
+      filename: 'hsf.jpg',
+      path: 'public\\temp\\hsf.jpg',
+      size: 74030
+    }
+  ]
+}
+TypeError: Cannot read properties of undefined (reading '0')
+    at file:///C:/Users/AashishGulshan/Desktop/New%20folder/backend/src/controllers/user.controller.js:56:52
+    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+
+
+*/
