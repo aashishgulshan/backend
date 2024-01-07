067fb7e451951498ff0793b590e38580b171d76b
Aashish Gulshan <aashish.gulshan@zehntech.com>
2024-01-07 17:20:26 +0530

    Added Subscription Model

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,14 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema ({
-    subscriber:{
-        type: Schema.Types.ObjectId, //Refers to User Consumer
-        ref: "User"
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, //Refers to User Consumer
+      ref: "User",
     },
     channel: {
-        type: Schema.Types.ObjectId, // refers to Content Provider
-        ref: "User"
-    }
-},{timestamps: true})
+      type: Schema.Types.ObjectId, // refers to Content Provider
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Subscription = mongoose.model("Subscription", subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
