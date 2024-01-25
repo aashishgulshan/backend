48ca67e214fa6b7264beded37923ed5b7aae90d4
Aashish Gulshan <aashish.gulshan@zehntech.com>
2024-01-25 19:13:39 +0530

    Added multiple routes and controller for update user details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,6 @@ import {
   updateUserAvatar,
   updateUserCoverImage,
   testUser,
-
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -38,6 +37,16 @@ router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
 
-router.route("/upate").post(updateUser);
+// router.route("/upate").post(updateUser);
+
+router.route("/change-password").post(changeCurrentPassword);
+
+router.route("/whoiam").get(getCurrentUser);
+
+router.route("/update-account-details").post(updateAccountDetails);
+
+router.route("/update-profile").post(updateUserAvatar);
+
+router.route("/update-cover-image").post(updateUserCoverImage);
 
 export default router;
